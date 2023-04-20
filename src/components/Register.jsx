2db93e99c41fb2bb41d4026f0ef4a6a91bc649ff
rajibrahman74e <rajiburrahman74@gmail.com>
2023-04-20 14:14:15 +0600

    [ADD]-register-form-on-register-components

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import React from "react";
 
 const Register = () => {
-    return (
-        <div>
-            <h2>This is Register page</h2>
+  return (
+    <div className="max-w-7xl mx-auto px-4 hero min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse justify-evenly">
+        <div className="w-[50%] card flex-shrink-0 max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+          </form>
         </div>
-    );
+        <div className="w-[50%] text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Register here!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
